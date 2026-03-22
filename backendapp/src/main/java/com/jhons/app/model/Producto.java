5eeeb5445020ba26a02985de40930ad4e9de17f4
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-22 15:32:31 -0500

    feat: Initialize Spring Boot backend application with Maven wrapper and a basic Producto model.

---
+++ backendapp/src/main/java/com/jhons/app/model/Producto.java
@@ -0,0 +1,13 @@
+package main.java.com.jhons.app.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Producto {
+  private Long id;
+  private String nombre;
+  private Double precio;
+  private Integer stock;
+}
