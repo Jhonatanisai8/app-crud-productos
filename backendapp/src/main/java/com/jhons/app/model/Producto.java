f62bfab4f956f1fae3ced72091b7c232998b7475
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-22 15:33:15 -0500

    feat: create `Producto` JPA entity model and add VS Code Java null analysis setting.

--- backendapp/src/main/java/com/jhons/app/model/Producto.java
+++ backendapp/src/main/java/com/jhons/app/model/Producto.java
@@ -1,13 +1,36 @@
-package main.java.com.jhons.app.model;
+package com.jhons.app.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "productos")
 public class Producto {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(nullable = false)
   private String nombre;
+
+  @Column(nullable = false)
   private Double precio;
+
+  @Column(nullable = false)
   private Integer stock;
+
+  private String categoria;
 }
