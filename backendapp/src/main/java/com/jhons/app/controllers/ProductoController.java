c308fd7fcd0341b026f66e96a5d394a6ed25db7e
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-22 15:41:00 -0500

    feat: add `ProductoController` with endpoints for listing and creating products.

---
+++ backendapp/src/main/java/com/jhons/app/controllers/ProductoController.java
@@ -0,0 +1,36 @@
+package com.jhons.app.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jhons.app.dto.ProductoDTO;
+import com.jhons.app.service.ProductoService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/productos")
+@RequiredArgsConstructor
+public class ProductoController {
+
+  private final ProductoService productoService;
+
+  @GetMapping
+  public ResponseEntity<List<ProductoDTO>> obtenerTodos() {
+    return ResponseEntity.ok(productoService.obtenerTodos());
+  }
+
+  @PostMapping
+  public ResponseEntity<ProductoDTO> crear(@Valid @RequestBody ProductoDTO productoDTO) {
+    return new ResponseEntity<>(productoService.crear(productoDTO), HttpStatus.CREATED);
+  }
+
+}
